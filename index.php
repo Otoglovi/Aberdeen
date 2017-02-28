c8de43362e2b853128c0e599a3ec67e081248b76
Otoglovi <darlington.2004@gmail.com>
2017-02-28 19:50:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<p>
+    <?php
+    $myname = "Kay";
+    $myage = 12;
+
+    if ($myage == 16){
+        print "Welcome ". $myname. ". You are only allowed to buy specs";
+    }
+    else if ($myage == 18){
+        print "Welcome ". $myname. ". You are only allowed to buy mugs";
+    }
+    else if ($myage == 21){
+        print "Welcome ". $myname. ". You are only allowed to buy sausage rolls";
+    }
+    else{
+        print "Welcome ". $myname. ". Sorry!!! You are under age. You cannot buy 
+        anything in Aberdeen";
+    }
+    ?>
+</p>
+</body>
+</html>
